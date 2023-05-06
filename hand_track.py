d9c0ce67074deaab57e44d3e5a0b5be535ea012b
Zach <exzachlyme@gmail.com>
2023-05-06 13:48:20 -0400

    hand_track.py: completed program with moving average

--- hand_track.py
+++ hand_track.py
@@ -5,11 +5,18 @@
 import cv2
 import mediapipe as mp
 import imutils
+import pyautogui
+
+MOVING_AVERAGE_LEN  = 5
+SCALE_FACTOR_X      = 1
+SCALE_FACTOR_Y      = 1
 
 cam = cv2.VideoCapture(0)
 mpHands = mp.solutions.hands
 hands = mpHands.Hands()
 mpDraw = mp.solutions.drawing_utils
+
+mpos = [(0,0) for i in range(MOVING_AVERAGE_LEN)]
 
 while True:
     ret,img = cam.read()
@@ -21,10 +28,22 @@ while True:
             for i,lm in enumerate(handLms.landmark):
                 h,w,c = img.shape
                 cx,cy = int(lm.x * w), int(lm.y * h)
-
+                
                 cv2.circle(img, (cx,cy), 10, (255, 0, 255), cv2.FILLED)
 
             mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+
+        mx = pyautogui.size()[0] - pyautogui.size()[0]*handLms.landmark[0].x
+        my = pyautogui.size()[1]*handLms.landmark[0].y
+
+        mpos.append((mx*SCALE_FACTOR_X, my*SCALE_FACTOR_Y))
+        mpos.pop(0)
+
+        mx = sum([x for (x,y) in mpos])/MOVING_AVERAGE_LEN
+        my = sum([y for (x,y) in mpos])/MOVING_AVERAGE_LEN
+
+        pyautogui.moveTo(mx, my)
+
 
     cv2.imshow("Hands", img)
     k = cv2.waitKey(1)
